6fcd7f8710a00e940681680260097b2d260839cb
Khushal Vaghasiya <khushalvaghasiya0@gmail.com>
2025-05-21 18:05:37 +0530

    Navbar updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,80 +1,66 @@
 import './Header.css';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
-import { FaComments } from 'react-icons/fa';
-import headerlogo  from '../assets/images/white-logo.png';
+import headerlogo from '../assets/images/Header-logo.jpg';
 
 const Header = () => {
-<<<<<<< HEAD
-    return (
-        <>
-    <h1>hello</h1>
-           
-        </>
-    )
-=======
   return (
-    <>
-      <Navbar bg="light" expand="lg" className="py-3">
-        <Container>
-          <Navbar.Brand href="#">
-            <img
-              src={headerlogo}
-            //   width="30px"
-            //   height="30px"
-              className="d-inline-block align-top me-2"
-              alt="Solarex logo"
-            />
-          </Navbar.Brand>
+    <Navbar expand="lg" className="custom-navbar">
+      <Container>
+        {/* Left Section - Logo */}
+        <Navbar.Brand href="#">
+          <img
+            src={headerlogo}
+            height="90"
+            className="d-inline-block align-top"
+            alt="Solarex logo"
+          />
+        </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="navbar-nav" />
-          <Navbar.Collapse id="navbar-nav">
-            <Nav className="ms-auto align-items-center">
-              <NavDropdown title="Home" id="home-dropdown">
-                <NavDropdown.Item href="#home/1">Home 1</NavDropdown.Item>
-                <NavDropdown.Item href="#home/2">Home 2</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#about">About Us</Nav.Link>
-              <NavDropdown title="Services" id="services-dropdown">
-                <NavDropdown.Item href="#services/1">
-                  Service 1
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#services/2">
-                  Service 2
-                </NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="Projects" id="projects-dropdown">
-                <NavDropdown.Item href="#projects/1">
-                  Project 1
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#projects/2">
-                  Project 2
-                </NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="Blogs" id="blogs-dropdown">
-                <NavDropdown.Item href="#blogs/1">Blog 1</NavDropdown.Item>
-                <NavDropdown.Item href="#blogs/2">Blog 2</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="Pages" id="pages-dropdown">
-                <NavDropdown.Item href="#pages/1">Page 1</NavDropdown.Item>
-                <NavDropdown.Item href="#pages/2">Page 2</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+        {/* Toggle Button for Mobile */}
+        <Navbar.Toggle aria-controls="navbar-nav" />
 
-              {/* Help section with phone number */}
-              <div className="d-flex align-items-center ms-4">
-                <FaComments size={20} className="me-2" />
-                <div className="d-flex flex-column">
-                  <small className="text-muted">Need help?</small>
-                  <strong>(307) 555-0133</strong>
-                </div>
-              </div>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+        {/* Center Nav */}
+        <Navbar.Collapse id="navbar-nav" className="justify-content-center">
+          <Nav className="header-center d-flex align-items-center">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+
+            <NavDropdown title="Solar" id="solar-dropdown">
+              <NavDropdown.Item href="#solar/overview">
+                Overview
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#solar/solutions">
+                Solutions
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link href="#ev-charging">EV Charging</Nav.Link>
+
+            <NavDropdown title="Knowledge Center" id="knowledge-dropdown">
+              <NavDropdown.Item href="#knowledge/blogs">Blogs</NavDropdown.Item>
+              <NavDropdown.Item href="#knowledge/videos">
+                Videos
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link href="#partner">Partner with Us</Nav.Link>
+
+            <NavDropdown title="About Us" id="about-dropdown">
+              <NavDropdown.Item href="#about/company">Company</NavDropdown.Item>
+              <NavDropdown.Item href="#about/team">Team</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+
+        {/* Right Section - Reach Us Button */}
+        <div className="header-right d-none d-lg-block">
+          <a href="tel:+918123410101" className="reach-btn">
+            Reach us : (+91) 8123410101
+          </a>
+        </div>
+      </Container>
+    </Navbar>
   );
->>>>>>> 684e649fcbae0547d285405e015b14987962ef4a
 };
 
 export default Header;
