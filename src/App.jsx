b2a2901e5183fcd7e2dd9a3f4d416f79bc23dd01
Khushal Vaghasiya <khushalvaghasiya0@gmail.com>
2025-05-21 15:48:49 +0530

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
 
 function App() {
 
   return (
     <>
-      <h2>Hello World</h2>
+      <Header/>
     </>
   );
 }
